35783a9b8007af9b30c5f3a4a0488fe6d169f84e
Chris <122332721@umail.ucc.ie>
2023-03-30 00:16:31 +0100

    File Tidy

--- Assets/Tileset/First Asset pack.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="First Asset pack" tilewidth="32" tileheight="32" tilecount="144" columns="12">
- <image source="First Asset pack.png" trans="575957" width="384" height="384"/>
-</tileset>
