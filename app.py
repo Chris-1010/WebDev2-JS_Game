2f67a288a55f32619c9189ad151212d1e4ff7ac4
Chris <122332721@umail.ucc.ie>
2023-04-14 00:31:57 +0100

    Polishing to Non-Game Files + Improvements

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = 'key'
 @app.route("/")
 def index():
     db = get_db()
-    players = db.execute("SELECT * FROM players;").fetchall()
+    players = db.execute("SELECT * FROM players ORDER BY score DESC;").fetchall()
     return render_template("index.html", players=players)
 
 @app.route("/play")
@@ -18,8 +18,13 @@ def game():
 
 @app.route("/store_score", methods=["POST"])
 def store_score():
+    name = request.form["name"]
     score = int(request.form["score"])
     enemy_count = int(request.form["enemies_killed"])
     time_alive = request.form["time_alive"]
+
+    db = get_db()
+    db.execute("INSERT INTO players (name, score, enemy_count, time_alive) VALUES (?, ?, ?, ?);", (name, score, enemy_count, time_alive))
+    db.commit()
 
     return "success"
